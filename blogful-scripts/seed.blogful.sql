43b742b36b54ae66bf8043ba4dc62e29cc1d15eb
Nishat Hoque <hoquen0397@gmail.com>
2021-02-06 17:26:29 -0500

    blogful create and seed sql

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('The World is Collapsing',                         now() - '21 days'::INTERVAL,    'The world is predicted to collapse tomorrow. ha ha'),
+    ('Hello Kitty World is opening',                    now() - '19 days'::INTERVAL,    'Hello Kitty World opening decided to be in the year 3000'),
+    ('Airpods OMG',                                     now() - '18 days'::INTERVAL,    'Airpods have gained such popularity there is almost no one in this planet without one'),
+    ('Gum Is Watermelon',                               now() - '17 days'::INTERVAL,    null),
+    ('Coffee Banned in sevral Countries',               now() - '15 days'::INTERVAL,    'Coffee the blood of college students have been banned in several countries today'),
+    ('Price of Tea Skyrockets',                         now() - '15 days'::INTERVAL,    'Prices of tea have nearly doubled after coffee was recently banned'),    
+    ('DisneyWorld Tickets decrease in Price',           now() - '14 days'::INTERVAL,    null), 
+    ('Mars Determined To Be Toxic',                     now() - '13 days'::INTERVAL,    null),
+    ('Laptops Exploding Everywhere',                    now() - '11 days'::INTERVAL,    null),
+    ('Starbucks Bankrupt!',                             now() - '10 days'::INTERVAL,    'After many years of exploding popularity, Starbucks has gone bankrupt after the recent coffee ban'),
+    ('Samsung Takes over Expedition Research',          now() - '9 days'::INTERVAL,    null),
+    ('Pants May Only be Red, New Decree',               now() - '8 days'::INTERVAL,    'As Trump comes back to power, we have all been decreed to only wear red pants'),
+    ('Thousands Arrested "Jeans Forever They State!"',  now() - '7 days'::INTERVAL,    'Millions of college students across the nation protest the coffee ban while wearing jeans.'),
+    ('Underground Factories Raided',                    now() - '6 days'::INTERVAL,    null),
+    ('Coffee Ban May be Reversed',                      now() - '5 days'::INTERVAL,    'After the death of hundreds of colleg students the Trump administration seems to be contemplating the reversal of the coffee ban'),
+    ('Teavanna Lobbyists Bribe Judge?!',                now() - '4 days'::INTERVAL,    null),
+    ('Gum Wrappers Used to Create Pants',               now() - '3 days'::INTERVAL,    null),
+    ('New Research Indicate Jupitar May Be Viable',     now() - '2 days'::INTERVAL,    null),
+    ('Yellow is no longer a Color',                     now() - '1 days'::INTERVAL,    null),                    
+    ('HELLO MELLO',                                     now(),                         'YELLOW')
+;
